fde8cdc34568d71c6818a381cce3a4bc26f52f21
Iago Lima <iagoclima22@gmail.com>
2026-02-02 21:03:34 -0300

    Duracao.java - Exercício Resolvido

---
+++ src/Duracao.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Duracao {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite a duracao em segundos: ");
+		int duracao = sc.nextInt();
+		
+		int horas = duracao / 3600;
+		int minutos = duracao % 3600 / 60;
+		int segundos = duracao % 3600 % 60;
+		System.out.printf("%d:%d:%d", horas, minutos, segundos);
+		
+		sc.close();
+	}
+
+}

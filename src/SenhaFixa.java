836b4dfc98e2b0ae2162c020266f2012b5414c2f
Iago Campanhol de Lima <iagoclima22@gmail.com>
2026-02-05 19:03:21 -0300

    SenhaFixa.java - Exercício Resolvido

---
+++ src/SenhaFixa.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SenhaFixa {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int senha;
+		
+		System.out.print("Digite a senha: ");
+		senha = sc.nextInt();
+		
+		while (senha != 2002) {
+			System.out.print("Senha invalida! Tente novamente: ");
+			senha = sc.nextInt();
+		}
+		
+		System.out.println("Acesso permitido!");
+		
+		sc.close();
+	}
+}
